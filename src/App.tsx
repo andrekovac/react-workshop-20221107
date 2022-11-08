19cf0365343d73468a8040d8ae8cbbdcaaa27a96
André Kovac <me@andrekovac.com>
2022-11-08 12:32:56 +0100

    App.tsx demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const MeinParagraph = () => {
+  return (
+    <p className="my-class" style={{ backgroundColor: "blue" }}>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {1 === 1 ? <MeinParagraph /> : <div>bla bla</div>}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +27,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
