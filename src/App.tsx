8cec348a3ac1dd58883c7c1728b81f62342d79fe
André Kovac <me@andrekovac.com>
2022-11-09 09:45:09 +0100

    event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import SimpleN from "./components/SimpleName";
 
+const handleClick = (text: string) => alert(text);
+
 const App = () => {
+  const text = "Zeige das hier an.";
+
   return (
     <div className="App">
       <SimpleN />
+      <button onClick={() => handleClick(text)}>Hier klicken</button>
     </div>
   );
 };
